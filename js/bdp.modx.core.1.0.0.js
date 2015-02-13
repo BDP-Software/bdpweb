282845438ddbb0791eb59ea432c8e408ff8d9b52
mbarcroft <michael@barcroftdigital.com>
2015-02-13 20:29:35 +0000

    update to core js

--- js/bdp.modx.core.1.0.0.js
+++ js/bdp.modx.core.1.0.0.js
@@ -193,27 +193,6 @@ $(document).ready(function(){
     })
 
 
-    /* map property */
-    $('a[href="#location"]').on('shown.bs.tab', function(){
-        $("#map-property").gmap3({
-            map: {
-                options: {
-                  center: [55.956357,-2.776160],
-                  zoom: 13,
-                  scrollwheel: false
-                }  
-             },
-            marker:{
-                latLng: [55.956357,-2.776160],
-                options: {
-                 icon: new google.maps.MarkerImage(
-                   "https://dl.dropboxusercontent.com/u/29545616/Preview/location.png",
-                   new google.maps.Size(48, 48, "px", "px")
-                 )
-                }
-             }
-        });
-    })
-	
+   
 	
 	
